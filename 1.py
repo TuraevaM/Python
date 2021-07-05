9608e5ce8359abf2510e5f57d4aa2cde6a9ac6ff
TuraevaM <86949062+TuraevaM@users.noreply.github.com>
2021-07-05 12:22:02 +0500

    python
    123456

---
+++ 1.py
@@ -0,0 +1,41 @@
+lugat={
+      "olma" : 15000,
+      "gilos" : 5000,
+      "shaftoli" : 10000,
+      "nok": 20000,
+      "qovun" : 12000,
+      "tarvuz" : 20000
+      }
+for i, j in lugat.items():
+      print(f"{i} - {j} so'm")
+n=true      
+while n==true:
+      print("qaysi meva kerak (x - hech qaysi)")
+      m=input()
+      if m=="x":
+            n=true
+      else:
+            n=false
+      
+
+
+
+
+
+"""
+n=1
+s=0
+while n==1:
+      print("Qaysi meva kerak?(x-xech qaysi)")
+      n=input()
+      if n=="x":
+            print(f" Summa {s}")
+            break
+      else:
+            c=input()
+            print("Necha kilo kerak?")
+            b=int(input())
+"""         
+            
+            
+            
